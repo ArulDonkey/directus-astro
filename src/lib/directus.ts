841b083edd056580168de5a0b7b6848582026323
ArulDonkey <pahaerulm@gmail.com>
2025-08-27 17:09:27 +0700

    Request Data from Directus

---
+++ src/lib/directus.ts
@@ -0,0 +1,41 @@
+import { createDirectus, rest, } from "@directus/sdk";
+
+type Benefit ={
+    desc: string;
+    judul: string;
+    logoBenefit: File,
+}
+
+type Visi = {
+    judul: string;
+    desc: string;
+}
+
+type Kurikulum = {
+    judul: string;
+    desc: string;
+}
+
+type Bidang = {
+    judul: string;
+    logoBidang: File;
+}
+
+type Fasilitas = {
+    judul: string;
+    desc: string;
+}
+
+type Schema = {
+    benefit: Benefit[];
+    visi: Visi[];
+    kurikulum: Kurikulum[];
+    bidang: Bidang[];
+    fasilitas: Fasilitas[];
+}
+
+
+
+const directus = createDirectus<Schema>('http://localhost:8055').with(rest());
+
+export default directus;
